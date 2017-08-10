59ebfd93c40f7c196f40e4a7a3fa879d148ce005
Vishesh Jindal <vishesh@hartleylab.com>
2017-08-10 07:57:30 +0530

    header content and flex style

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return (
         <Card>
-        <CardSection>
-        <Text>{props.album.title}</Text>
-        </CardSection>
+            <CardSection>
+                <View style={styles.headerContentStyle}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'sapce-around'
+    }
 };
 
 export default AlbumDetail;
