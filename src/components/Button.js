38ed5d4bc44f504fe75adb85e7ad9bff0b48b700
Vishesh Jindal <vishesh@hartleylab.com>
2017-08-11 08:37:29 +0530

    added onPress on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,37 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+    const { buttonStyle, textStyle } = styles;
     return (
-        <TouchableOpacity>
-            <Text>Click Me</Text>
+        <TouchableOpacity style={buttonStyle} onPress={() => console.log('pressed')}>
+            <Text style={textStyle}>
+                Click Me
+            </Text>
         </TouchableOpacity>
     );
+};
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingBottom: 10,
+        paddingTop: 10,
+        elevation: 3
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#ffffff',
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5,
+        elevation: 2
+    }
 };
 
 export default Button;
